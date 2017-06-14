a1a8e093decfe07fddff210c784f699eeec0959b
luisvalesilva <luisvalesilva@gmail.com>
2017-06-14 18:31:57 -0400

    Handle different reference genomes

--- RNA-seq_sacCer3_slurm_job.sh
+++ RNA-seq_sacCer3_slurm_job.sh
@@ -4,7 +4,7 @@
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=16GB
 #SBATCH --time=2:00:00
-#SBATCH --job-name=RNAseq_sacCer3
+#SBATCH --job-name=RNAseq_analysis
 #SBATCH --mail-type=FAIL
 #SBATCH --mail-user=lv38@nyu.edu
 #SBATCH --output=/scratch/%u/%x_%j.out
@@ -14,42 +14,77 @@
 #                                INSTRUCTIONS                                  #
 #------------------------------------------------------------------------------#
 # Dependencies:
-#     - S288C sacCer3 genome reference: .fasta and .gff
+#     - Genome reference: .fasta and .gff
 #     - Bowtie2 index
 #       (Assumed to be in '~/Library/Bowtie2_sacCer3')
 
-### RUN AS:
-# sbatch --export EXPID="for_output_files",RUNDIR="path/to/dir",\
-# FQ="absolute/path/to/reads.fastq",\
-# ~/Pipeline/Slurm/RNA-seq-sacCer3.sh
+### Argument options:
+# EXPID     Custom ID for output files
+# RUNDIR    Path to directory to run script and save output in
+# FQ        Absolute path to input fastq file
+# GENDIR    Absolute path to directory containing reference genome files.
+#           Must include:
+#               FASTA file
+#               Matching GFF file
+#           An existing Bowtie2 index with a basename ("bt2_base") matching the
+#           FASTA file name is used if found in the same directory; otherwise a
+#           new index is built
 
 ### EXAMPLE:
-# sbatch --export EXPID="AH119_3h",RUNDIR="scratch/lv38",\
-# T_MAP="scratch/lv38/C8C2NACXX_l03n01_ah119-3-030316.3510000004e291.fastq" \
-# ~/Pipeline/Slurm/RNA-seq-sacCer3.sh
+# sbatch --export EXPID="AH119_3h",RUNDIR="/scratch/lv38",\
+# FQ="/scratch/lv38/C8C2NACXX_l03n01_ah119-3-030316.3510000004e291.fastq",\
+# GENDIR="/home/lv38/Library/SK1Yue" ~/Pipeline/Slurm/RNA-seq-sacCer3.sh
 
 #------------------------------------------------------------------------------#
 
-echo "Started pipeline:"
+echo ">>>>> Started pipeline:"
 date
-cd ${RUNDIR}
 
-# Abort if output directory already exists
-if [ -d "${EXPID}" ]
+# Find reference genome files
+FA=$(find $GENDIR -iname "*.fa*")
+GFF=$(find $GENDIR -iname "*.gff")
+
+# Exit if not found
+if [ -z "$FA" ] || [ -z "$GFF" ]
 then
-    echo "Directory already exists"; exit 1;
+    echo ">>>>> Could not find reference genome files"; exit 1;
 fi
 
-mkdir ${EXPID}/
-cd ${EXPID}/
+# Find Bowtie2 index (is there a file named as "fasta_base_name.1.bt2"?)
+IX=$(basename $FA)                              # Drop path to file
+IX=${IX%.*}                                     # Drop extension
+CHECKIX=$(find $GENDIR -iname "${IX}.1.bt2")    # Search file
+#IX=$(basename $IX | cut -d '.' -f 1)
+
+# Build Bowtie2 index if not found
+if [ -z "$CHECKIX" ]
+then
+    echo ">>>>> Building Bowtie2 index"
+    module purge
+    module load bowtie2/intel/2.2.9
+    # Build index
+    cd $GENDIR
+    bowtie2-build -f $FA $IX
+fi
+
 
 #--------------------------------------------#
 # Map reads to reference genome with TopHat2 #
 #--------------------------------------------#
+cd $RUNDIR
 
+# Abort if output directory already exists
+if [ -d "$EXPID" ]
+then
+    echo ">>>>> Directory already exists"; exit 1;
+fi
+
+mkdir ${EXPID}/
+cd ${EXPID}/
 mkdir ${EXPID}_sacCer3_TopHat2-nnjuncs/
 
-echo "Map reads with TopHat2..."
+echo ">>>>> Map reads with TopHat2..."
+module purge
 module load tophat/intel/2.1.1
 module load bowtie2/intel/2.2.9
 module load samtools/intel/1.3.1
@@ -58,15 +93,15 @@ tophat2 -p 8 \
     --no-novel-juncs \
     --library-type fr-firststrand \
     -o ${EXPID}_sacCer3_TopHat2-nnjuncs/ \
-    -G ~/Library/Bowtie2_sacCer3/S288C_chr_copy.gff \
-    ~/Library/Bowtie2_sacCer3/sacCer3 ${FQ}
+    -G $GFF \
+    $GENDIR/$IX $FQ
 
 
 #---------------------------------#
 # Sort and index BAM file for IGV #
 #---------------------------------#
 
-echo "Sort and index bam file for IGV..."
+echo ">>>>> Sort and index bam file for IGV..."
 module purge
 module load samtools/intel/1.3.1
 
@@ -78,17 +113,17 @@ samtools index ${EXPID}_sacCer3_TopHat2-nnjuncs/${EXPID}-accepted_hits_s.bam
 # Count reads with featureCounts (Subread package) #
 #--------------------------------------------------#
 
-echo "Count reads with featureCounts..."
+echo ">>>>> Count reads with featureCounts..."
 module purge
 module load subread/intel/1.5.1
 
 featureCounts -s 2 \
     -t CDS \
     -g Name \
-    -a ~/Library/Bowtie2_sacCer3/S288C_chr_copy.gff \
+    -a $GFF \
     -o ${EXPID}_sacCer3_TopHat2-nnjuncs/${EXPID}_featureCounts.txt \
     ${EXPID}_sacCer3_TopHat2-nnjuncs/accepted_hits.bam
 
 
-echo "Completed pipeline:"
+echo ">>>>> Completed pipeline:"
 date
